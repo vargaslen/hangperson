ed21744f71fd839daf1f6d4d3c89e1d78f88d93c
lenin <vargaslen@gmail.com>
2018-02-20 13:32:00 -0430

    cc

--- app.rb
+++ app.rb
@@ -44,8 +44,8 @@ class HangpersonApp < Sinatra::Base
     #la sentencia before ha capturado la instancia de juego en @game
     adivina_valida = @game.guess(letter)
     flash[:message] = "Adivinanza no válida." unless adivina_valida == true
-    flash[:message] = "Ya probaste con esa letra" if (@game.guesses.include?(letter) || @game.wrong_guesses.include?(letter))
-    #redirect '/show'                #origen
+    #flash[:message] = "Ya probaste con esa letra" if (@game.guesses.include?(letter) || @game.wrong_guesses.include?(letter))
+    redirect '/show'                #origen
     params.to_s
 
   end
