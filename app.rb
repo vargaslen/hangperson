a4689c5ce6dfecf55eba7c0aa6b5f505887bd801
lenin <vargaslen@gmail.com>
2018-02-20 13:24:51 -0430

    ccc

--- app.rb
+++ app.rb
@@ -38,17 +38,17 @@ class HangpersonApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    #letter = params[:guess].to_s[0]
-    
+    letter = params[:guess].to_s[0] #origen
+
     ### YOUR CODE HERE ###
     #la sentencia before ha capturado la instancia de juego en @game
-    #adivina_valida = @game.guess(letter)
-    #flash[:message] = "Adivinanza no válida." unless adivina_valida == true
-    #flash[:message] = "Ya probaste con esa letra" if (@game.guesses.include?(letter) || @game.wrong_guesses.include?(letter))
-    #redirect '/show'
+    adivina_valida = @game.guess(letter)
+    flash[:message] = "Adivinanza no válida." unless adivina_valida == true
+    flash[:message] = "Ya probaste con esa letra" if (@game.guesses.include?(letter) || @game.wrong_guesses.include?(letter))
+    redirect '/show'                #origen
     params.to_s
 
-  end
+  end 
 
   # Everytime a guess is made, we should eventually end up at this route.
   # Use existing methods in HangpersonGame to check if player has
