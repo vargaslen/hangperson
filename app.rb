8a8d457df3ac2cd997402e5dc493726b22a8bd63
lenin <vargaslen@gmail.com>
2018-02-20 14:34:54 -0430

    instance_of?

--- app.rb
+++ app.rb
@@ -41,10 +41,8 @@ class HangpersonApp < Sinatra::Base
     letter = params[:guess].to_s[0] #origen
 
     ### YOUR CODE HERE ###
-    @game = session[:game] || HangpersonGame.new('')
-    return @game.to_s
     #la sentencia before ha capturado la instancia de juego en @game
-    #adivina_valida = @game.guess(letter)
+    adivina_valida = @game.guess(letter) if @guess.instance_of?(HangpersonGame)
     #flash[:message] = "Adivinanza no válida." unless (adivina_valida == true)
     #flash[:message] = "Ya probaste con esa letra" if (@game.guesses.include?(letter) || @game.wrong_guesses.include?(letter))
     #redirect '/show'                #origen
