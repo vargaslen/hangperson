356da912aa198423a760c377d56e2bbbec1aa4e7
lenin <vargaslen@gmail.com>
2018-02-26 09:51:33 -0430

    rescue ArgumentError

--- app.rb
+++ app.rb
@@ -44,8 +44,11 @@ class HangpersonApp < Sinatra::Base
     ### YOUR CODE HERE ###
     #la sentencia before ha capturado la instancia de juego en @game
     flash[:message] = "Ya probaste con esa letra" if (@game.guesses.include?(letter) || @game.wrong_guesses.include?(letter))
-    adivina_valida = @game.guess(letter)
-    #flash[:message] = "Adivinanza no válida." unless (adivina_valida == true)
+    @game.guess(letter)
+  rescue ArgumentError
+      flash[:message] = "Adivinanza no válida."
+  end
+   
     #p "params.to_s:#{flash[:message]}"
     #redirect '/show'                #origen
 
